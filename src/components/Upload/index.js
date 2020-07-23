97ec768326abc265113b46f75c9ac34f0f68d98a
padmasritumati <tumatipadmasri98@gmail.com>
2020-07-23 20:16:36 +0200

    added a from in become a sitter and also and image upload

---
+++ src/components/Upload/index.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import { CloudinaryContext, Image } from "cloudinary-react";
+import { fetchPhotos, openUploadWidget } from "../../CloudinaryService";
+
+function Upload() {
+
+  const [images, setImages] = useState()
+
+  const beginUpload = tag => {
+    const uploadOptions = {
+      cloudName: "dsuvhhlxm",
+      tags: [tag, 'anImage'],
+      uploadPreset: "vnqxy7xe"
+    };
+    openUploadWidget(uploadOptions, (error, photos) => {
+      if (!error) {
+        console.log(photos);
+        if(photos.event === 'success'){
+          setImages([...images, photos.info.public_id])
+        }
+      } else {
+        console.log(error);
+      }
+    })
+  }
+
+  useEffect( () => {
+    fetchPhotos("image", setImages);
+  }, [])
+
+  return (
+    <CloudinaryContext cloudName="dsuvhhlxm">
+    <div >
+        <button onClick={() => beginUpload("image")}>Upload Image</button>
+      <section>
+        {images.map(i => <Image
+              key={i}
+              publicId={i}
+              fetch-format="auto"
+              quality="auto"
+            />)}
+      </section>
+    </div>
+    </CloudinaryContext>
+  
+  );
+}
+export default Upload;
--- src/pages/BecomeSitter/index.js
+++ src/pages/BecomeSitter/index.js
@@ -1,11 +1,66 @@
-import React from "react"
+import React,{useState} from "react";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+//import Upload from "../../components/Upload";
 
-export default function BecomeSitter(){
+export default function BecomeSitter() {
+ 
   return (
     <div>
-      <h1>
-      BecomeSitter
-      </h1>
+      <h1>BecomeSitter</h1>
+      <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-5">
+        <h1 className="mt-5 mb-5">Let's get started :)</h1>
+        <Form.Group controlId="formGridAddress1">
+          <Form.Label>House number</Form.Label>
+          <Form.Control placeholder="1234 Main St" />
+        </Form.Group>
+
+        <Form.Group controlId="formGridAddress2">
+          <Form.Label>street</Form.Label>
+          <Form.Control placeholder="Apartment, studio, or floor" />
+        </Form.Group>
+
+        <Form.Row>
+          <Form.Group as={Col} controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>postcode</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Country</Form.Label>
+            <Form.Control as="select" defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Control>
+          </Form.Group>
+        </Form.Row>
+
+        {
+          //<Upload></Upload>
+        }
+
+        <h3 className="mt-5 mb-3">Add your phone number</h3>
+        <p>
+          We requires a verified phone number for important updates. <br></br>
+          Note:
+          your phone number won't be displayed on your profile.
+        </p>
+
+        <Form.Group as={Col} controlId="formGridZip" type="number">
+            <Form.Label>Phone Number </Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
