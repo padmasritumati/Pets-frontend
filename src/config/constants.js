7fb32f251fededad0b8ae1cbecd02f1e722953dc
padmasritumati <tumatipadmasri98@gmail.com>
2020-10-04 22:33:54 +0200

    changes in .env

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,8 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 export const DEFAULT_PAGINATION_LIMIT = 10;
 export const apiKeyGoogle = process.env.REACT_APP_GOOGLE_API;
+export const ENV = process.env.NODE_ENV || "development";
+export const apiUrl =
+  ENV === "production"
+    ? "https://pets2020.herokuapp.com"
+    : "http://localhost:4000";
